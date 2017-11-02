a75483545d7d6d5a6f8697f7fb34b92a2fd2c594
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-02 16:11:58 +0530

    progressTree is now a progressGraph

--- src/components/main_container.js
+++ src/components/main_container.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import TopButtons from './top_buttons'
 import BottomButtons from './bottom_buttons'
-import ProgressTree from './tree'
+import ProgressGraph from './graph'
 
 class MainContainer extends Component {
   render () {
     return (
       <div className="col s9">
         <TopButtons />
-        <ProgressTree />
+        <ProgressGraph />
         <BottomButtons />
       </div>
     )

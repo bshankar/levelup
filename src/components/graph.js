b0bcb28eca492bb2be239bb4b5d41661cb303829
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-02 16:12:34 +0530

    add a basic support for clicking nodes

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,37 +1,62 @@
 import React, { Component } from 'react'
 import { Layer, Text, Stage, Arrow, Line } from 'react-konva'
+import ReactTooltip from 'react-tooltip'
 
-const statusToColors = {
-  'locked': 'grey',
-  'unlocked': 'lightgreen'
-}
-
-class GraphNode extends Component {
-  state = { status: 'locked' }
-
-  render() {
-    return (
-      <Text
-        x={this.props.x}
-        y={this.props.y}
-        text={this.props.text}
-        fontSize={this.props.size}
-        fill={statusToColors[this.state.status]}
-      />
-    )
+function createNode (title, description, size, status) {
+  return {
+    title: title,
+    description: description,
+    size: size,
+    status: status
   }
 }
 
-class GraphEdge extends Component {
+function getRequirements(node, graph) {
+  
+}
+
+const nodes = [
+  createNode('JS', 'Javascript journey starts here!', 64, 'start'),
+  createNode('EJS', 'Eloquent Javascript chapters 1 to 6', 28, 'unlocked'),
+  createNode('HRK', 'Get hackerrank rank in algorithms section below 10k', 22, 'unlocked'),
+  createNode('Project', 'Do a simple project', 35, 'locked'),
+  createNode(':42', 'Search for the meaning of life', 25, 'locked')
+]
+
+const sampleGraph = [[1, 2], [3], [3], [4]]
+
+const statusToColors = {
+  'root': 'goldenrod',
+  'locked': 'grey',
+  'unlocked': 'lightgreen',
+  'progress': 'green',
+  'finished': 'gold'
+}
+
+class GraphNode extends Component {
+  state = { status: this.props.initialStatus || 'locked'}
+
+  handleClick = () => {
+    switch (this.state.status) {
+    case 'unlocked':
+      this.setState({status: 'progress'})
+      break
+    case 'progress':
+      this.setState({status: 'finished'})
+      break
+    }
+  }
+  
   render() {
     return (
-      <Line
-        pointerLength={10}
-        pointerWidth={10}
-        strokeWidth={this.props.thickness}
-        points={this.props.points}
-        fill={'lightgrey'}
-        stroke={'lightgrey'} />
+        <Text
+          x={this.props.x}
+          y={this.props.y}
+          text={this.props.text}
+          fontSize={this.props.size}
+          fill={statusToColors[this.state.status]}
+          onClick={this.handleClick}
+        />
     )
   }
 }
@@ -41,25 +66,11 @@ class ProgressGraph extends Component {
     return (
       <Stage width={700} height={700}>
         <Layer>
-        <GraphNode x={350} y={100} text={'Js'} size={64} />
-        <GraphNode x={350} y={200} text={'Eloquent'} size={28} />
-        <GraphNode x={250} y={200} text={'Hrk'} size={22} />
-        <GraphNode x={200} y={250} text={'Node'} size={18} />
-        <GraphNode x={300} y={250} text={'Ejs'} size={22} />
-        <GraphNode x={400} y={250} text={'ES6'} size={18} />
-        <GraphNode x={500} y={250} text={'Project'} size={35} />
-        <GraphNode x={350} y={320} text={'Big Project'} size={38} />
-
-        <GraphNode x={100} y={450} text={'Frontend'} size={48} />
-        <GraphNode x={250} y={410} text={'HTML/CSS'} size={18} />
-        <GraphNode x={200} y={360} text={'React'} size={25} />
-
-        <GraphNode x={500} y={450} text={'DBMS'} size={48} />
-        <GraphNode x={510} y={400} text={'Redis'} size={25} />
-        <GraphNode x={400} y={400} text={'MongoDB'} size={18} />
-
-        <GraphEdge points={[380, 150, 300, 200]} />
-        <GraphEdge points={[380, 150, 400, 200]} />
+          <GraphNode x={350} y={360} text={nodes[0].title} size={nodes[0].size} initialStatus='root' />
+          <GraphNode x={450} y={280} text={nodes[1].title} size={nodes[1].size} initialStatus='unlocked'/>
+          <GraphNode x={250} y={280} text={nodes[2].title} size={nodes[2].size} initialStatus='unlocked'/>
+          <GraphNode x={320} y={200} text={nodes[3].title} size={nodes[3].size} />
+          <GraphNode x={350} y={120} text={nodes[4].title} size={nodes[4].size} />
         </Layer>
       </Stage>
     )

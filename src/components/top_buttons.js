9788b9bea6a2bd2e85042a91b8a004b5455d849b
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-03 12:23:01 +0530

    add a few buttons

--- src/components/top_buttons.js
+++ src/components/top_buttons.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 class TopButtons extends Component {
   render () {
     return (
-      <div style={{position: 'absolute', right: '0.5em', top: '0.5em'}}>
-        <button className="btn waves-effect waves-light btn-flat">Logout
-        </button>
+        <div style={{position: 'absolute', right: '0.5em', top: '0.5em'}}>
+        <button className="btn waves-effect waves-light btn-flat">All Trophies (0)</button>
+        <button className="btn waves-effect waves-light btn-flat">Level 0</button>
+        <button className="btn waves-effect waves-light btn-flat">Logout</button>
       </div>
     )
   }

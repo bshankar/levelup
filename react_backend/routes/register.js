9838e30d3a3bdd31ff0c5282cd319cc51d36792d
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-16 14:55:18 +0530

    add function to register a new user on the server

--- react_backend/routes/register.js
+++ react_backend/routes/register.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var redis = require('redis')
 var bcrypt = require('bcrypt')
-
 var router = express.Router()
 
 var redisClient = redis.createClient()
@@ -10,7 +9,31 @@ redisClient.on('connect', function () {
 })
 
 router.post('/', function (req, res, next) {
-  // register user
+  redisClient.hmget('user:' + req.body.username,
+                    'password',
+                    'active',
+                    function (err, dbres) {
+                      if (err) res.send('database error')
+
+                      const hash = dbres[0]
+                      if (hash !== null) res.send('user already exists.')
+                      else {
+                        bcrypt.genSalt(10, function (err, salt) {
+                          if (err) res.send(err.message)
+                          bcrypt.hash(req.body.password, salt, function (err, h) {
+                            if (err) res.send(err.message)
+                            redisClient.hmset('user:' + req.body.username,
+                                              'password', h,
+                                              'active', '1',
+                                              function (dberr, dbres) {
+                                                if (dberr) res.send(dberr)
+                                                res.send('ok')
+                                              }
+                            )
+                          })
+                        })
+                      }
+                    })
 })
 
 module.exports = router

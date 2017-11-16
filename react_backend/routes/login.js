c56d2afec71988b0b07efcf401cc28561d140531
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-16 14:24:15 +0530

    compare cryptographic hash instead of the password during login

--- react_backend/routes/login.js
+++ react_backend/routes/login.js
@@ -16,12 +16,16 @@ router.post('/', function (req, res, next) {
                     function (err, dbres) {
                       if (err) res.send('database error')
 
-                      const pass = dbres[0]
+                      const hash = dbres[0]
                       const active = dbres[1]
-                      if (pass !== null && pass === req.body.password && active !== '0') {
-                        res.send('ok')
+                      if (hash !== null && active !== '0') {
+                        bcrypt.compare(req.body.password, hash, function (berr, bres) {
+                          if (berr) res.send(berr.message)
+                          if (bres === true) res.send('ok')
+                          else res.send('password is wrong')
+                        })
                       } else if (active === '0') res.send('this account is closed')
-                        else res.send('invalid username/password')
+                        else res.send('user doesn\'t exist')
                     })
 })
 

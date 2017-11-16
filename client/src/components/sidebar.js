ca88e525ba66cc74df259b635e224cfb95ca0eb8
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-16 11:21:40 +0530

    open should be set to this.props.open

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -5,7 +5,7 @@ import TextField from 'material-ui/TextField'
 class SideBar extends Component {
   render () {
     return (
-      <Drawer type="persistent" open={this.props.loggedin}>
+      <Drawer type="persistent" open={this.props.open}>
         <TextField label="Search" style={{marginLeft: '1em', marginRight: '1em'}}></TextField>
       </Drawer>
     )

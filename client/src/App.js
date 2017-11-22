24a06b397348bc7e25cfe25a430afadd254d52c7
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-22 11:48:19 +0530

    add a form to rightbar in edit mode

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Drawer from 'material-ui/Drawer'
 import Typography from 'material-ui/Typography'
 import Divider from 'material-ui/Divider'
 import Button from 'material-ui/Button'
+import { FormControl, FormHelperText } from 'material-ui/Form'
+import TextField from 'material-ui/TextField'
 import axios from 'axios'
 
 import SideBar from './components/sidebar'
@@ -50,17 +52,31 @@ class App extends Component {
   render () {
     const currentNode = this.state.currentNode
     const mainContainerClass = 'col ' + (currentNode !== null ? 's6' : 's10')
-    
-    const rightBarJsx = currentNode !== null ? <div style={{margin: '0.5em'}}>
-          <Typography type="title"> {currentNode.id} </Typography>
-          <p/>    
-          <Typography type="body1"> {currentNode.description} </Typography>
-          <p/><p/>
-          <Button onClick={this.handleStatusClick}> {currentNode.status} </Button>
-          <Divider light />
-          <Typography type="title"> Comments </Typography>
-          <Comments currentNode={currentNode} addComment={this.addComment} />
-          </div> : <p/>
+
+    let rightBarJsx = <p/>
+    if (this.state.currentNode !== null) {
+      if (this.state.mode === 'readonly') {
+        rightBarJsx = <div style={{margin: '0.5em'}}>
+                        <Typography type="title"> {currentNode.id} </Typography>
+                        <p/>    
+                        <Typography type="body1"> {currentNode.description} </Typography>
+                        <p/><p/>
+                        <Button onClick={this.handleStatusClick}> {currentNode.status} </Button>
+                        <Divider light />
+                        <Typography type="title"> Comments </Typography>
+                        <Comments currentNode={currentNode} addComment={this.addComment} />
+                      </div>
+      } else {
+        rightBarJsx = <form style={{"margin": "0.5em"}}>
+                        <FormControl fullwidth>
+                          <TextField label="title" value={currentNode.id}></TextField>
+                          <TextField label="description" value={currentNode.description} multiline margin="normal"></TextField>
+                        </FormControl>
+                        <br/>
+                        <Button raised>save</Button>
+                      </form>
+      }
+    }
 
     let mainContainerJsx = <Typography type="title">Loading...</Typography>
     if (this.state.loggedin === null) mainContainerJsx = <LoginRegisterForm afterLogin={this.openUserHome} />

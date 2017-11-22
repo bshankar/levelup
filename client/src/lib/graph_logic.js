cf9690e112c774b34fe90d977209054325696290
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-22 15:45:23 +0530

    add logic for adding and deleting nodes from the graph

--- client/src/lib/graph_logic.js
+++ client/src/lib/graph_logic.js
@@ -25,4 +25,58 @@ function handleStatusClick () {
   }
 }
 
-export {tryUnlockingNexts, handleStatusClick}
+function nextUniqueId () {
+  this.state.idCounter++
+  return 'id' + this.state.idCounter
+}
+
+function addNode () {
+  const newNode = {
+    id: nextUniqueId(),
+    description: 'some description here',
+    weight: 25,
+    status: 'locked',
+    comments: []
+  }
+  this.state.graph.nodes.push(newNode)
+  this.state.graph.edges.push([])
+  this.state.graph.deps.push([])
+  this.setState({currentNode: newNode})
+}
+
+function addEdge (index1, index2) {
+  this.state.graph.edges[index1].push(index2)
+  this.state.graph.deps[index2].push(index1)
+  this.setState({graph: this.state.graph})
+}
+
+function deleteEdge (index1, index2) {
+  this.state.graph.edges[index1].splice(index2, 1)
+  this.state.graph.deps[index2].splice(index1, 1)
+  this.setState({graph: this.state.graph})
+}
+
+function addChild () {
+  const index1 = this.state.graph.nodes.indexOf(this.state.currentNode)
+  addNode()
+  const index2 = this.state.graph.nodes.length - 1
+  addEdge(index1, index2)
+}
+
+function deleteNode () {
+  const i = this.state.graph.nodes.indexOf(this.state.currentNode)
+  this.state.graph.nodes.splice(i, 1)
+  this.state.graph.edges.splice(i, 1)
+  this.state.graph.deps.splice(i, 1)
+  this.setState({currentNode: null})
+}
+
+export {
+  tryUnlockingNexts,
+  handleStatusClick,
+  addNode,
+  addEdge,
+  addChild,
+  deleteNode,
+  deleteEdge
+}

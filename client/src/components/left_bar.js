410a0832774df352da86188a46e79c8fe95e53a7
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-23 15:51:40 +0530

    hardcode some graph names on the left_bar

--- client/src/components/left_bar.js
+++ client/src/components/left_bar.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import Drawer from 'material-ui/Drawer'
 import TextField from 'material-ui/TextField'
+import List, {ListItem, ListItemText} from 'material-ui/List'
+import Button from 'material-ui/Button'
 
 class SideBar extends Component {
   render () {
     return (
       <Drawer type="persistent" open={this.props.open}>
         <TextField label="Search" style={{marginLeft: '1em', marginRight: '1em'}}></TextField>
+        <List style={{marginTop: '1em'}}>
+          <ListItem button>JavaScript</ListItem>
+          <ListItem button>Clojure</ListItem>
+          <ListItem button>Python</ListItem>
+          <Button raised style={{margin: '1em'}}>add new</Button>
+        </List>
       </Drawer>
     )
   }

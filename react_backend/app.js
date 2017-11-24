b3f67637c3fe2d1fd520e463ef3269bc3e4d1bd6
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-24 11:19:26 +0530

    do not expose session secret in the code

--- react_backend/app.js
+++ react_backend/app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(session({resave: true, saveUninitialized: true, secret: 'sign_cookie_with_me', cookie: { maxAge: null }}))
+app.use(session({resave: true, saveUninitialized: true, secret: process.env.SECRET, cookie: { maxAge: null }}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)

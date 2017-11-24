c771421bb9d9c8947d877a2742390c50c24cfd3a
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-24 11:28:31 +0530

    use jade to show error, home pages on the proxy

--- react_backend/app.js
+++ react_backend/app.js
@@ -14,6 +14,10 @@ const users = require('./routes/users')
 const dashboard = require('./routes/dashboard')
 const app = express()
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
